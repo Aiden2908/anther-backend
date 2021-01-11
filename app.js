8f32b735da1197f3c8840fa6a832552cb2267039
iMac <imac@iMacs-iMac.local>
2021-01-11 21:24:07 +1300

    chnaged socket.io version to 2.1.1'

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+// const server = require("http").createServer(app);
+// const io = require("socket.io")(server);
 const PORT_CHAT = 3000;
 
 //.env
@@ -28,9 +28,17 @@ app.listen(PORT, () => {
 });
 
 //Chat
-io.on("connection", (socket) => {
-  console.log("a user has connected :)");
+// io.on("connection", (socket) => {
+//   console.log("a user has connected :)");
+//   socket.emit("chat-message", "hello (from-server)");
+// });
+var server = require("http").createServer();
+var io = require("socket.io")(server);
+io.on("connection", function (client) {
+  client.on("event", function (data) {});
+  client.on("disconnect", function () {});
 });
+// server.listen(3000);
 
 server.listen(PORT_CHAT, () => {
   console.log(`Server(chat) is running on port ${PORT_CHAT}`);
